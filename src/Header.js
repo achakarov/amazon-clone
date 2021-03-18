50cdad03260f2a310274d1cb82c08f85f9428806
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-03-18 18:16:32 +0200

    updatd classnames

--- src/Header.js
+++ src/Header.js
@@ -13,15 +13,15 @@ function Header() {
             <div className="header_nav">
                 <div className="header_option">
                     <span className="header_optionLineOne">Hello Guest</span>
-                    <span className="header_optionLineOne">Sign in</span>
+                    <span className="header_optionLineTwo">Sign in</span>
                 </div>
                 <div className="header_option">
                     <span className="header_optionLineOne">Returns</span>
-                    <span className="header_optionLineOne">& Orders</span>
+                    <span className="header_optionLineTwo">& Orders</span>
                 </div>
                 <div className="header_option">
                     <span className="header_optionLineOne">Your</span>
-                    <span className="header_optionLineOne">Prime</span>
+                    <span className="header_optionLineTwo">Prime</span>
                 </div>
             </div>
         </div>
