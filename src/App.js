c477b678fef8b40c6c8d2351acc0d394223d4eaa
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-03-18 18:12:41 +0200

    created partially header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello world!</h1>
+      {/* Header */}
+      <Header />
+      {/* Home */}
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+
+function Header() {
+    return (
+        <div className="header">
+            <img className="header_logo" src="https://logos-world.net/wp-content/uploads/2020/04/Amazon-Symbol.jpg" alt="" />
+            <div className="header_search">
+                <input type="text" className="header_searchInput" />
+                <SearchIcon className="header_searchIcon" />
+            </div>
+            <div className="header_nav">
+                <div className="header_option">
+                    <span className="header_optionLineOne">Hello Guest</span>
+                    <span className="header_optionLineOne">Sign in</span>
+                </div>
+                <div className="header_option">
+                    <span className="header_optionLineOne">Returns</span>
+                    <span className="header_optionLineOne">& Orders</span>
+                </div>
+                <div className="header_option">
+                    <span className="header_optionLineOne">Your</span>
+                    <span className="header_optionLineOne">Prime</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
