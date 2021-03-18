b8884eeb41cd28a87bb3608a81629f715c494424
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-03-18 17:32:49 +0200

    added firebase config

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAJ4xO0LRjzzqE4gbefWg5HwINv4IcS29M",
+    authDomain: "clone-5621f.firebaseapp.com",
+    projectId: "clone-5621f",
+    storageBucket: "clone-5621f.appspot.com",
+    messagingSenderId: "1034752615213",
+    appId: "1:1034752615213:web:765aa0fb517a18a5aea9a4"
+};
