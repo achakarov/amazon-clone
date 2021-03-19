059d505e275be2fd86b957bbd8a6960c72bdad38
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-03-19 16:46:28 +0200

    created background img on home page

---
+++ src/Home.js
@@ -0,0 +1,25 @@
+import './Home.css';
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="home_container">
+                <img className="home_image" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/gateway/placement/launch/Coming2America/C2A2_2021_GWBleedingHero_FT_COVIDUPDATE_XSite_1500X600_PV_en-GB._CB659358842_.jpg" alt="" />
+                <div className="home_row">
+                    {/* Product  */}
+                    {/* Product  */}
+                </div>
+                <div className="home_row">
+                    {/* Product  */}
+                    {/* Product  */}
+                    {/* Product  */}
+                </div>
+                <div className="home_row">
+                    {/* Product  */}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
