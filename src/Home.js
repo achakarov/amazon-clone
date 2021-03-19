1ff27e0f6e74d7d8687df06e63830ba5c5aaa02a
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-03-19 17:18:44 +0200

    added dynamically props

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,7 @@ function Home() {
             <div className="home_container">
                 <img className="home_image" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/gateway/placement/launch/Coming2America/C2A2_2021_GWBleedingHero_FT_COVIDUPDATE_XSite_1500X600_PV_en-GB._CB659358842_.jpg" alt="" />
                 <div className="home_row">
-                    <Product />
+                    <Product title='The lean startup' price={19.99} image='https://images-na.ssl-images-amazon.com/images/I/51CTIr1bJxL._SX325_BO1,204,203,200_.jpg' rating={5}/>
                     <Product />
                 </div>
                 <div className="home_row">
--- src/Product.js
+++ src/Product.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import './Product.css';
 
-function Product() {
+function Product({ title, image, price, rating }) {
     return (
         <div className="product">
             <div className="product_info">
-                <p>The lean startup</p>
+                <p>{title}</p>
                 <p className="product_price">
                     <small>$</small>
-                    <strong>19.99</strong>
+                    <strong>{price}</strong>
                 </p>
                 <div className="product_rating">
-                    <p>&#11088;</p>
+                    {Array(rating).fill().map((_, i) => (<p>&#11088;</p>))}
                 </div>
             </div>
-            <img src="https://images-na.ssl-images-amazon.com/images/I/51CTIr1bJxL._SX325_BO1,204,203,200_.jpg" alt="" />
+            <img src={image} alt="" />
             <button>Add to basket</button>
         </div>
     );
