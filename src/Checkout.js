4f460db6a2881e20649c42d7a571bcbeb37f2223
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-03-27 20:46:42 +0200

    added Links in the navbar

--- src/Checkout.js
+++ src/Checkout.js
@@ -4,7 +4,21 @@ import './Checkout.css';
 function Checkout() {
   return (
     <div className="checkout">
-      <h1>smash</h1>
+      <div className="checkout-left">
+        <img
+          className="checkout-ad"
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+          alt=""
+        />
+        <div>
+          <h2 className="checkout-title">your shopping basket</h2>
+          {/* BasketItem */}
+        </div>
+      </div>
+
+      <div className="checkout-right">
+        <h2>the subtotal will go here</h2>
+      </div>
     </div>
   );
 }
