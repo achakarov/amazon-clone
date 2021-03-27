703735414fa85beca1e01fa5aec8e9d0aec86d20
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-03-27 20:30:43 +0200

    added route to checkout page

---
+++ src/Checkout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Checkout.css';
+
+function Checkout() {
+  return (
+    <div className="checkout">
+      <h1>smash</h1>
+    </div>
+  );
+}
+
+export default Checkout;
